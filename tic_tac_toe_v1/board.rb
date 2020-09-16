0a41d74432f957f11030e30ffe4d9b894298776e
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-17 00:54:20 +0200

    Check is valid? position

--- tic_tac_toe_v1/board.rb
+++ tic_tac_toe_v1/board.rb
@@ -1,9 +1,16 @@
 class Board
+    ROWS = 3
+    COLS = 3
+    SIZE = ROWS * COLS
     def initialize
-        @board = [
+        @grid = [
                   ['_', '_', '_'],
                   ['_', '_', '_'],
                   ['_', '_', '_']
                 ]
+    end
+
+    def valid?(position)
+        return position[0] < ROWS && position[1] < COLS
     end
 end
