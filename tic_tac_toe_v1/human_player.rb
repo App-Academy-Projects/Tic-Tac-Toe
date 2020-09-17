f5f8822128a8749f718ec287b2296ec597fe9fab
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-17 02:42:30 +0200

    Get position from user

--- tic_tac_toe_v1/human_player.rb
+++ tic_tac_toe_v1/human_player.rb
@@ -4,5 +4,11 @@ class HumanPlayer
         @mark = mark_value
     end
 
-    
+    def get_position
+        print "Player #{self.mark}, enter two numbers representing a position in the format `row col`: "
+        input = gets.chomp.split
+        raise "Invalid positionm must be in format `row col`" if input.size != 2
+        row, col = input
+        return [row.to_i, col.to_i]
+    end
 end
