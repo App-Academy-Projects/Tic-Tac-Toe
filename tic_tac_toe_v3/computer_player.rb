bb2e867939757296ee97c64297776d47600aca56
Usama Fouad <usamaeltmsah@gmail.com>
2020-09-17 14:07:37 +0200

    Create a ComputerPlayer class to play againest the computer :D

---
+++ tic_tac_toe_v3/computer_player.rb
@@ -0,0 +1,14 @@
+require './human_player.rb'
+
+class ComputerPlayer
+    def initialize(mark_value)
+        @mark = mark_value
+    end
+
+    def get_position(legal_positions)
+        len = legal_positions.size
+        pos = legal_positions[rand(0...len)]
+        puts "Computer #{@mark} chose position #{pos}"
+        return pos
+    end
+end
